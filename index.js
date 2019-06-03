bb59eace3c2695b692618099f8fa16d40e35693d
jmhill91 <john.m.hill91@gmail.com>
2019-06-03 19:45:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  coolDom()
+} );
+
+
+
+function coolDom() {
+  document.getElementById("text").innerHTML = "This is really cool!";
+}
